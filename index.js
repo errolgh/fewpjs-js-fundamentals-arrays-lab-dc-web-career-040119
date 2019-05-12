9da272b9897c2f123aa9f6451aee81c8b0ac3c8a
errolgh <ewatsonrei@gmail.com>
2019-05-12 15:47:13 -0400

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Write your solution here!
-const append = ['Milo', 'Otis', 'Garfield', 'Odie'];
+const append = ["Milo", "Otis", "Garfield"]
+append.push('odie')
 
 const prepend = ['Odie', 'Milo', 'Otis', 'Garfield']
 
